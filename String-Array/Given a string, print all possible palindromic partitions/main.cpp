9af156f695a8a0014f163ab0eacb8de9e7db354b
Oleksii Pavlov <Pyrotex.ffimd.workflow@gmail.com>
2017-10-03 15:40:22 +0300

    edit task (Alpha)

--- String-Array/Given a string, print all possible palindromic partitions/main.cpp
+++ String-Array/Given a string, print all possible palindromic partitions/main.cpp
@@ -1,10 +1,10 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 //#include <iostream>
 //#include <cstring>
 
 using namespace std;
 
-int palPrint (char *arr, int begin = -1, int end = -1);
+int palPrint(char *arr, int begin = -1, int end = -1);
 
 int main()
 {
@@ -16,81 +16,120 @@ int main()
 
     palPrint(word);
 
-    //if (((wordlen) % 2) != 0)
-    {
-        int mid = (int)((double)wordlen / 2.0);
-        int origMid = mid;
-        int _i = mid + 1;
+    int mid = (int)((double)wordlen / 2.0);
+    int center = mid;
+    int _i = 0;
+    int i = 0;
+    bool con = false;
 
+    //if ((wordlen % 2) == 0)
         if (word[mid] == word[mid - 1])
-        palPrint(word, mid - 1, mid);
+            palPrint(word, mid - 1, mid);
 
-        while (mid < wordlen - 1)
-        {
-            for (int i = mid - 1; _i < wordlen; i--, _i++)
-            {
-                cout << i << " " << _i << endl;
-                if (word[i] == word[_i])
-                    palPrint(word, i, _i);
-                else
-                    if (word[mid] == word[mid + 1])
-                        palPrint(word, mid, mid + 1);
-                    else
-                        break;
-            }
-            mid++;
-        }
-
-        mid = origMid - 1;
+    while (mid < wordlen - 1)
+    {
+        //for (int i = mid - 1; _i < wordlen; i--, _i++)
+        i = mid - 1;
         _i = mid + 1;
-        while (mid > 0)
+        con = true;
+        while ((i >= 0) && (_i <= wordlen - 1) && con)
         {
-            for (int i = mid - 1; i >= 0; i--, _i++)
+            cout << i << " " << _i << endl;
+            if ((word[mid] == word[mid + 1]) && con)
             {
-                cout << i << " " << _i << endl;
-                if (word[i] == word[_i])
-                    palPrint(word, i, _i);
-                else
-                    if (word[mid] == word[mid - 1])
-                        palPrint(word, mid - 1, mid);
-                    else
-                        break;
+                palPrint(word, mid, mid + 1);
+                con = false;
             }
-            mid--;
+            if (word[i] == word[_i])
+            palPrint(word, i, _i);
+            else
+                break;
+        i--;
+        _i++;
         }
-
-        cout << endl;
+        mid++;
     }
+
+    //if ((wordlen % 2) == 0)
+    //    mid = center - 1;
+    //else
+    mid = center - 1;
+    _i = 0;
+    i = 0;
+    while (mid > 0)
+    {
+        //for (int i = mid - 1; _i < wordlen; i--, _i++)
+        i = mid - 1;
+        _i = mid + 1;
+        con = true;
+        while ((i >= 0) && (_i <= wordlen - 1))
+        {
+            cout << i << " " << _i << endl;
+            if ((word[mid - 1] == word[mid]) && con)
+            {
+                palPrint(word, mid - 1, mid);
+                con = false;
+            }
+            if (word[i] == word[_i])
+            palPrint(word, i, _i);
+            else
+                break;
+        i--;
+        _i++;
+        }
+        mid--;
+    }
+
+    if ((wordlen % 2) == 0)
+    {
+        _i = wordlen - 1;
+        i = 0;
+        bool palindrom = true;
+        while (i < _i)
+        {
+            if (word[i] != word[_i])
+            {
+                break;
+                palindrom = false;
+            }
+            i++;
+            _i--;
+        }
+        if (palindrom)
+            palPrint(word, 0, wordlen - 1);
+    }  
+
+    cout << endl;
 
     return 0;
 }
 
-int palPrint (char *arr, int begin, int end)
+int palPrint(char *arr, int begin, int end)
 {
     int wordlen = strlen(arr);
     if (begin == -1)
-    for (int i = 0; i < wordlen; i++)
+        for (int i = 0; i < wordlen; i++)
+        {
+            if (wordlen != end)
+                cout << arr[i] << " ";
+        }
+    else if ((begin > -1) && (end > begin) && (end < wordlen) && (begin < wordlen))
     {
-        if (wordlen != end)
-        cout << arr[i] << " ";
+        for (int i = 0; i < begin; i++)
+            cout << arr[i] << " ";
+
+        for (int i = begin; i < end; i++)
+            cout << arr[i];
+
+        for (int i = end; i < wordlen; i++)
+            cout << arr[i] << " ";
     }
     else
-        if ((begin > -1) && (end > begin) && (end < wordlen) && (begin < wordlen))
-        {
-            for (int i = 0; i < begin; i++)
-                cout << arr[i] << " ";
-
-            for (int i = begin; i < end; i++)
-                cout << arr[i];
-
-            for (int i = end; i < wordlen; i++)
-                cout << arr[i] << " ";
-        }
-        else
-        {
-            cerr << endl << "Incorect argue in palPrint" << endl;
-            return -1;
-        }
+    {
+        cerr << endl
+             << "Incorect argue in palPrint" << endl;
+        return -1;
+    }
 
     cout << endl << endl;
 }
